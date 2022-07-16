be00b58303f75d57843b7db86893cf2073889ef4
Nagy-Zsugya Martin Albert <zsugya.martin@gmail.com>
2022-07-16 15:27:16 +0200

    Implement drawing on hover when mouse is clicked

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ for(let i = 0; i < 16 * 16; i++){
     tempDiv.classList.add('single-block');
     drawingBoard.appendChild(tempDiv);
 }
+const gridDivs = document.querySelectorAll('.single-block');
+
+window.addEventListener('mousedown', (e) => {
+    gridDivs.forEach(gridElement => {
+        gridElement.addEventListener('mouseover', addListener);
+    });
+});
+
+window.addEventListener('mouseup', (e) => {
+    gridDivs.forEach(gridElement => {
+        gridElement.removeEventListener('mouseover', addListener);
+    });
+});
+
+function addListener(e){
+    e.target.classList.add('colored');
+}
